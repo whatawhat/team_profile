c439c1bf213759338d7e97155e61a5a7911e282d
Stephanie <whatawhat@hotmail.com>
2021-06-21 21:01:42 -0400

    created src directory and added generateTeamProfile.js to it

---
+++ src/generateTeamProfile.js
@@ -0,0 +1,17 @@
+// TODO: Create a function that states what to show based on answers from list
+function switchRoles (role) {
+    if (role === "Engineer") {
+
+
+    } else if (role === "Intern") {
+
+    } else {
+
+    }
+  };
+  
+ 
+  // TODO: Create a function to generate markdown for README
+    
+  module.exports = generateProfile;
+  
