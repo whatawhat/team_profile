999ada22a1ffeb83416d4bfd13284210b8002cdb
Stephanie <whatawhat@hotmail.com>
2021-07-01 13:46:15 -0400

    team.html is the file I'm testing bootstrap on; created functions for engineer and intern card and added bootstrap

--- src/generateTeamProfile.js
+++ src/generateTeamProfile.js
@@ -3,11 +3,59 @@
 const inquirer = require("inquirer");
 
 function generateManagerCard (manager) {
-  return `<div>
-    <h1>${manager.name}</h1>
-    <h2>${manager.id}</h2>
-    <h2>${manager.email}</h2>
-    <h2>${manager.officeNumber}</h2>
+  return `<div class="col custom-card mb-5">
+  <div class="card shadow">
+  <div class="card-header bg-secondary text-light">
+    <h3 class="card-title">${manager.name}</h3>
+    <i class="d-inline bi bi-mouse2"></i>
+    <h5 class="d-inline">${manager.getRole()}</h5>
+    </div>
+    <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${manager.id}</li>
+     <li class="list-group-item"><a href="mailto:${manager.getEmail()}">${manager.email}</a></li>
+     <li class="list-group-item">${manager.getOfficeNumber()}</li>
+     </ul>
+    </div>
+    </div>
+  </div>`
+}
+
+function generateEngineerCard(engineer) {
+  return `<div class="col custom-card mb-5">
+  <div class="card shadow">
+  <div class="card-header bg-secondary text-light">
+  <h3 class="card-title">${engineer.name}</h3>
+  <i class="d-inline bi bi-bricks"></i>
+  <h5 class="d-inline">${engineer.getRole()}</h5>
+  </div>
+  <div class="card-body">
+  <ul class="list-group list-group-flush">
+  <li class="list-group-item">${engineer.id}</li>
+     <li class="list-group-item"><a href="mailto:${engineer.getEmail()}">${engineer.email}</a></li>
+     <li class="list-group-item"><a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub}</a></li>
+     </ul>
+    </div>
+    </div>
+  </div>`      
+}
+
+function generateInternCard(intern) {
+  return `<div class="col custom-card mb-5">
+  <div class="card shadow">
+  <div class="card-header bg-secondary text-light">
+    <h3 class="card-title">${intern.name}</h3>
+    <i class="d-inline bi bi-eyeglasses"></i>
+    <h5 class="d-inline">${intern.getRole()}</h5>
+    </div>
+    <div class="card-body">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${intern.id}</li>
+     <li class="list-group-item"><a href="mailto:${intern.getEmail()}">${intern.email}</a></li>
+     <li class="list-group-item">${intern.getSchool()}</li>
+     </ul>
+    </div>
+    </div>
   </div>`
 }
 
@@ -17,22 +65,35 @@ function generateProfile (workers) {
   let templateArray = [];
   for (var i = 0; i < workers.length; i++) {
     if (workers[i].getRole() === "Manager") {
-      generateManagerCard
       templateArray.push(generateManagerCard(workers[i]))
     }
+    if (workers[i].getRole() === "Engineer") {
+      templateArray.push(generateEngineerCard(workers[i]))
+    }
+    if (workers[i].getRole() === "Intern") {
+      templateArray.push(generateInternCard(workers[i]))
+    }
   }
-  return `<!DOCTYPE html>
+  return `  
+  <!DOCTYPE html>
   <html lang="en">
   <head>
-      <meta charset="UTF-8">
-      <title>Team Profile Generator</title>
-      <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <title>Team Profile Generator</title>
   </head>
   <body>
-      <div>
-          <h1>My Team</h1>
-      </div>
+    <section class="container-fluid py-5 bg-info text-center text-light">
+      <h1>My Team</h1>
+    </section>
+      <div class="container mt-5">
+      <div class="row row-cols-1 row-cols-sm-2 rows-cols-md-3 g-4">
       ${templateArray.join("")}
+      </div>
+      </div>
+
   </body>
   
   </html>`
